6709b09cada4c3b9fbfd467af89771fc7c8089f8
Mahmudul Hassan <mahmudul.coder@gmail.com>
2024-10-14 13:34:45 +0600

    Logger, mongoose, jwt added

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata'; // We need this in order to use @Decorators
+
+import config from './config';
+
+import express from 'express';
+
+import Logger from './loaders/logger';
+
+async function startServer() {
+  const app = express();
+
+  /**
+   * A little hack here
+   * Import/Export can only be used in 'top-level code'
+   **/
+  await require('./loaders').default({ expressApp: app });
+
+  app
+    .listen(config.port, () => {
+      Logger.info(`
+      ################################################
+      🛡️  Server listening on port: ${config.port} 🛡️
+      ################################################
+    `);
+    })
+    .on('error', err => {
+      Logger.error(err);
+      process.exit(1);
+    });
+}
+
+startServer();
--- src/loaders/mongoose.js
+++
@@ -1,7 +0,0 @@
-import * as mongoose from "mongoose";
-export default async () => {
-  const connection = await mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-  });
-  return connection.connection.db;
-};
